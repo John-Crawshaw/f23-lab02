8237b614d9a46ee55aadcd93ca4fa59270ec64dc
John-Crawshaw <jccrawsh.andrew.cmu.edu>
2024-01-23 23:01:43 -0500

    Fixed encapsulation in all files

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,23 @@
 package edu.cmu.cs.cs214.lab02;
 
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Shape circle = new Circle(2);
+        Shape rectangle = new Rectangle(2, 3);
+        Shape square = new Square(29);
+        
 
         Renderer renderer = new Renderer(rectangle);
+        Renderer renderer2 = new Renderer(circle);
+        Renderer renderer3 = new Renderer(square);
 
         renderer.draw();
+        renderer2.draw();
+        renderer3.draw();
     }
 }
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,6 +1,15 @@
 import { newRenderer } from "./renderer.js"
-import { Rectangle, newRectangle } from "./shapes/rectangle.js";
+import { Rectangle} from "./shapes/rectangle.js";
+import { Shape} from "./shapes/shape.js";
+import { square} from "./shapes/square.js";
+import { circle} from "./shapes/circle.js";
 
-const rectangle: Rectangle = newRectangle(2, 3)
+const circle1: Shape = circle(2)
+const rectangle: Shape = Rectangle(2, 3)
+const square1: Shape = square(2)
 const renderer = newRenderer(rectangle)
+const renderer1 = newRenderer(circle1)
+const renderer2 = newRenderer(square1)
 renderer.draw();
+renderer1.draw();
+renderer2.draw();
